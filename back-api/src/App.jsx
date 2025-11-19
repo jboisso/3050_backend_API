e70140204f3b005e8c95bd71ba9fff0f9bb82698
Jonathan Boissonnas <jonathan.boissonnas@gmail.com>
2025-11-19 18:23:37 +0100

    Uebung 1

--- back-api/src/App.jsx
+++ back-api/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect, useState } from "react";
+
+export function App() {
+  const [data, setData] = useState({});
+  const [metadata, setMetadata] = useState({});
+  const [metalink, setMetalink] = useState("");
+
+  const layers = data?.layers || [];
+
+  useEffect(() => {
+    fetch("https://api3.geo.admin.ch/rest/services/api/MapServer")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => setData(res))
+      .catch((err) => console.error("Fetch failed:", err));
+  }, []);
+
+  useEffect(() => {
+    fetch(metalink)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => setMetadata(res))
+      .catch((err) => console.error("Fetch failed:", err));
+  }, [metalink]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>Ich bin ein Text</p>
+      <p>{metadata.layers?.[0]?.fullName}</p>
+      <ol>
+        {Object.entries(metadata.layers?.[0] || {}).map(([key, value], idx) => (
+          <li key={idx}>
+            <strong>{key}:</strong> {String(value)}
+          </li>
+        ))}
+      </ol>
+      <p>{metalink}</p>
+      <ol>
+        {layers.map((layer, idx) => (
+          <li key={idx}>
+            <div
+              onClick={() => {
+                setMetalink(
+                  `https://api3.geo.admin.ch/rest/services/api/MapServer?searchText=${layer.idGeoCat}`
+                );
+              }}
+            >
+              {layer.fullName}
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
-export default App
+export default App;
